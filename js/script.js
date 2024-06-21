5084b6f5e227a207d04839f87fc9a4a37124b73b
Ramen Biswas <133987263+devsramen@users.noreply.github.com>
2024-06-21 13:47:11 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.aboutUsSliderWrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+
+    // $('.planCards').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 8000,
+    //   });
+
+
+})
